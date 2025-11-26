3426e4bd7187c2ebd94021e9c6b2e1f6061470fa
Lene <Lene-tl@hotmail.com>
2025-11-26 12:54:28 +0100

    1/4

---
+++ ParProgramering_ModelYourself/Personer.cs
@@ -0,0 +1,15 @@
+﻿
+namespace ParProgramering_ModelYourself
+{
+    public class Personer
+    {
+        public string Name { get; set; }
+        public string Pronoun { get; set; }
+        public int Age { get; set; }
+        public string City { get; set; }
+        public string Hobby1 { get; set; }
+        public string Hobby2 { get; set; }
+        public string Hobby3 { get; set; }
+
+    }
+}
--- ParProgramering_ModelYourself/Program.cs
+++ ParProgramering_ModelYourself/Program.cs
@@ -4,10 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("test");
-            Console.WriteLine("test2rd");
-            Console.WriteLine("test3rd");
+            Personer person1 = new Personer { Name = "Remy", Pronoun = "Han", Age = 36, City = "Rennesøy", Hobby1="Trommer", Hobby2="Fiske", Hobby3="Motorsykkel"};
+            Personer person2 = new Personer { Name = "Lene", Pronoun = "Hu", Age = 27, City = "Tønsberg", Hobby1 = "Gaming", Hobby2 = "Baking", Hobby3="Koding" };
+            Console.WriteLine("Hei!");
+            Console.WriteLine($"Dette er {person1.Name}! \n{person1.Pronoun} er {person1.Age} år gammel, og kommer fra {person1.City}.\n{person1.Name}’s hobbyer er: \n{person1.Hobby1}, \n{person1.Hobby2}, \n{person1.Hobby3}");
+            Console.WriteLine($"Hvilken hobby vil du {person1.Name} skal gjøre?");
+            string RemyHobby = Console.ReadLine();
+            if (RemyHobby == "Trommer") 
+            {
+                Console.WriteLine("Remy slår løs på trommene!");
+            }
+            else if(RemyHobby == "Fiske")
+            {
+                Console.WriteLine("Remy står og fisker i ro og mak");
+            }
+            else if(RemyHobby == "Motorsykkel")
+            {
+                Console.WriteLine("Remy freser runt på to hjul");
+            }
+            else
+            {
+                Console.WriteLine($"Beklager {RemyHobby} er ikke en av Remy’s hobbyer. Han har bestemt seg for å ignorere deg.");
+            }
+            Console.WriteLine($"La oss møte {person2.Name}!");
+            Console.WriteLine($"Dette er {person2.Name}! \n{person2.Pronoun} er {person2.Age} år gammel, og kommer fra {person2.City}.\n{person2.Name}’s hobbyer er: \n{person2.Hobby1}, \n{person2.Hobby2}, \n{person2.Hobby3}");
+            
             Console.WriteLine("test4rd");
         }
     }
